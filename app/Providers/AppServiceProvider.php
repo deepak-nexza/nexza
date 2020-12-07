12712522d1e6c0cb8bbf43d6942c6b5b87309078
deepak sharma <deepak.sharma>
2020-12-07 23:58:41 +0530

    save

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -55,6 +55,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->app['request']->server->set('HTTPS', true);
         $this->setCustomUrlGenerator();
     }
 
